36ec4c1e299cdff8477c0f382a557c94eb0fa9b5
LokotoshKonstantin <Lokotoch777@mail.ru>
2018-12-22 21:00:08 +0300

    Add photo

--- app/admin/teams.rb
+++ app/admin/teams.rb
@@ -15,9 +15,25 @@ ActiveAdmin.register Team do
     actions
   end
 
+  show do
+    attributes_table do
+      row :name
+      row :photo do |t|
+        image_url = team.photo.url
+        image_tag image_url, size: '50x50' if image_url.present?
+      end
+      row :created_at
+    end
+  end
+
   form do |f|
     f.inputs do
       f.input :name
+      if f.object.photo.url.present?
+        f.input :photo, :as => :file, :hint => image_tag(f.object.photo.url, class: 'image-preview')
+      else
+        f.input :photo, :as => :file
+      end
     end
     f.actions
   end
